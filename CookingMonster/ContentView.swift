2ff34ed8463b20d7acc77476cf5cdbc3293795e8
hiroki kaneko <hrkknk@hirokinoMac-mini.local>
2020-01-16 03:20:30 +0900

    Initial Commit

---
+++ CookingMonster/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CookingMonster
+//
+//  Created by hiroki kaneko on 2020/01/16.
+//  Copyright © 2020 hiroki kaneko. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ CookingMonsterTests/CookingMonsterTests.swift
@@ -0,0 +1,34 @@
+//
+//  CookingMonsterTests.swift
+//  CookingMonsterTests
+//
+//  Created by hiroki kaneko on 2020/01/16.
+//  Copyright © 2020 hiroki kaneko. All rights reserved.
+//
+
+import XCTest
+@testable import CookingMonster
+
+class CookingMonsterTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
